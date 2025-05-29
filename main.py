352c2f157fe2ddd69db1333c8361f04ab718063c
Viz248 <vizbiz248@gmail.com>
2025-05-29 23:31:46 +0530

    Added CRUD

--- main.py
+++ main.py
@@ -25,15 +25,15 @@ class Task(BaseModel):
 
 tasks: List[Task]=[]    #tasks is a list that must be made up of class Task i.e. all contain id, title, is_done status
 
-@app.post("/tasks")
+@app.post("/tasks") #CREATE
 def create_task(task:Task):
     tasks.append(task)
     return task
 
-@app.get("/tasks")
+@app.get("/tasks")  #READ
 def get_tasks(done: Optional[bool]=None):   #Used to filter the done attribute in SwaggerUI. Nice
-    if done is None:    #None in this function basically acts as a default value and it IS a special value.
-        return tasks    #The reason we put Optional is because None isn't actually a bool value, but rather a special value.
+    if done is None:    #If there is NO filter given (True/False), then it is considered None and everything is shown
+        return tasks    #The reason we put Optional is because None isn't actually a bool value, but rather a special value 
     filtered_tasks=[]   #List comprehension (can also use return [task for task in tasks if tasks.done==done] i don't understand ew)
     for task in tasks:
         if task.done==done:
@@ -53,3 +53,21 @@ def get_tasks(task_id: int):
         #try/catch isn't really necessary here for exception handling since FastAPI does that FOR YOU already and displays it w/ JSON error codes.                                                    
         #Also we don't use else since that's only for successful stuff. Not errors lol.
 
+@app.put("/tasks/{task_id}")    #UPDATE
+def mark_done(task_id: int):
+    if tasks==[]:
+        raise HTTPException(status_code=404, detail="There are no tasks")
+    for task in tasks:
+        if task.id==task_id:
+            task.done=True
+            return task
+    raise HTTPException(status_code=404, detail="Task not found")
+
+@app.delete("/tasks/{task_id}") #DELETE
+def delete_task(task_id: int):
+    if tasks==[]:
+        raise HTTPException(status_code=404, detail="There are no tasks")
+    for i,task in enumerate(tasks): #Python equivalent to for(i=0,i<len(tasks),i++) lol
+        if task.id==task_id:
+            return tasks.pop(i)
+    raise HTTPException(status_code=404, detail="Task not found")   
