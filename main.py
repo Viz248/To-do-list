02637fbc791f5650facc2b01a3a43081f870c7bc
Viz248 <vizbiz248@gmail.com>
2025-06-02 02:13:08 +0530

    Added SQL db

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from routes import router
+from database import init_db
 
 app=FastAPI()
 app.include_router(router)
 
+init_db()   #best to put it after app starts
     
