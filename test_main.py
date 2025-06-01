741a2e7df59beb38b70570f3944879841e8e5fdc
Viz248 <vizbiz248@gmail.com>
2025-06-01 14:57:04 +0530

    Added PATCH unit testing

--- test_main.py
+++ test_main.py
@@ -8,3 +8,10 @@ def test_create_task():
     assert response.status_code==200
     assert response.json()["title"]=="Test task"
     assert response.json()["done"]==False
+
+def test_update_task():
+    response=client.post("/tasks",json={"title":"Test task"})
+    patch_response=client.patch("/tasks/1",json={"title":"Updated Test task"})
+    assert patch_response.status_code==200
+    assert patch_response.json()["title"]=="Updated Test task"
+    assert patch_response.json()["done"]==False
