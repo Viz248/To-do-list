aaf63d3993ebb6094810f56893db3c60d1b64c40
Viz248 <vizbiz248@gmail.com>
2025-06-02 23:11:12 +0530

    Added TaskBase, TaskRead. Fixed Task, TaskCreate.

--- models.py
+++ models.py
@@ -1,15 +1,22 @@
 from sqlmodel import SQLModel, Field #for data validation
+from pydantic import BaseModel
 from typing import Optional
 
-class Task(SQLModel, table=True):
-    id: Optional[int]=Field(default=None, primary_key=True)
+class TaskBase(SQLModel):   #DRY (Don't Repeat Everything). We reuse these a lot so just put it in a class and just use that
     title: str
+    
+class Task(TaskBase, table=True):
+    id: Optional[int]=Field(default=None, primary_key=True) #We put Optional since WE don't have it give it in SQLModel, the TABLE generates it for us by autoincrementing
     done: bool=False
-#so Task MUST have these 3 attributes and is_done is False by default
 
-class TaskCreate(BaseModel):    #This is the Task the client makes, they only need to enter the string
-    title: str
+class TaskCreate(TaskBase):
+    pass
 
-class UpdateTask(BaseModel):
+class UpdateTask(SQLModel):
     title: Optional[str]=None
     done: Optional[bool]=None
+
+class TaskRead(SQLModel):   #We have a seperate reading class for GET since class Task has id as Optional so FastAPI might think that it might not 
+    id: int                 #even exist at all which is NOT possible.
+    title: str              #response_model=List[Task] is the one that is responsible for data validation and hence gives the error
+    done: bool
