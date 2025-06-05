acf73b911f2af65c52db7f7f3487c8d1e36bf770
Viz248 <vizbiz248@gmail.com>
2025-06-06 00:24:45 +0530

    Split routes.py into tasks & auth

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from routes import router
+from routes.tasks import tasks_router
+from routes.auth import auth_router
 from database import init_db
 
 app=FastAPI()
-app.include_router(router)
+app.include_router(tasks_router, prefix="/tasks")   #if you have an endpoint w/ .post("login"), it'll actually be /tasks/login. 
+app.include_router(auth_router, prefix="/auth")     #This avoids collision and keeps things neat in SwaggerUI
 
 init_db()   #best to put it after app starts
     
