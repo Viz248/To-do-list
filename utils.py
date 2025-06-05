04ac739b6c77580ed9595f927fb3212425c39af7
Viz248 <vizbiz248@gmail.com>
2025-06-06 00:48:37 +0530

    Removed all auth-related functions.

--- utils.py
+++ utils.py
@@ -1,16 +1,8 @@
 #This file is for stuff like helper functions. This helps in reusability
 from sqlmodel import Session, select
 from models import Task
-from passlib.context import CryptContext    #for hashing, encoding/decoding, salting automatically. 
-                                            #O/w you'd have to include the whole do them with bcrypt everywhere
+
 
 def get_all_tasks(session: Session):
     return session.exec(select(Task)).all()
-
-pwd_context=CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def hash_password(password: str)->str:  #->str is used to show what the function is supposed to return the data type as. It's not needed but is a
-    return pwd_context.hash(password)   #good practice for readability
-
-def verify_password(password: str, hashed_password: str)->bool: #used when logging in
-    return pwd_context.verify(password, hashed_password)
+    
